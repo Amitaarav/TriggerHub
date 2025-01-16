06f9ec4831c9af7dfbd9376db9f6fc002ec2d0a6
[Amit Kumar] <[aaravamt555@gmail.com]>
2025-01-16 19:19:05 +0530

    second-commit

---
+++ hooks/src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+const app = express();
+
+const client = new PrismaClient();
+
+app.use(express.json());
+app.post("/hooks/catch/:userId/:zapId",async(req,res)=>{
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body
+
+    await client.$transaction(async(tx)=>{
+        const run = await tx.zapRun.create({
+            data:{
+                zapId: zapId,
+                metadata: body
+            }
+        });
+
+        await tx.zapRunOutbox.create({
+            data:{
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message:"webhook received"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000")
+})
